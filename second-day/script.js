6687604bd8e1dddc635a93c985d582a0a91733d7
MohammadAliAlizada <muhdalializada@gmail.com>
2023-04-06 04:07:37 +0500

    second day practice

---
+++ second-day/script.js
@@ -0,0 +1,26 @@
+// Complete the function solveMeFirst to compute the sum of two integers.
+
+// Example
+
+
+// Return .
+
+// Function Description
+
+// Complete the solveMeFirst function in the editor below.
+
+// solveMeFirst has the following parameters:
+
+// int a: the first value
+// int b: the second value
+// Returns
+// - int: the sum of  and 
+function solveMeFirst(a, b) {
+  // Hint: Type return a+b below   
+  if(a>=1 && b<=1000){
+      return a+b
+  }
+}
+
+const result = solveMeFirst(5,4)
+console.log(`The result of sum is = `,result)
